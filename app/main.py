c8d0e83882ec811cef23a40c0eef38f8907558ca
Egor <ifbytopic@gmail.com>
2025-09-01 16:16:25 +0700

    Improving the code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from app.routers.account import router
 
-app = FastAPI(title="Bank API", version="1.0.0")
+app = FastAPI(title="Bank API", version="1.1.1")
 app.include_router(router)
