3cb606c628327f054f11554dd58afa713e36680b
Egor <ifbytopic@gmail.com>
2025-09-01 16:00:27 +0700

    Changing logic and adding migrations

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from app.database import engine
-from app.models.account import Account
 from app.routers.account import router
-
-Account.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bank API", version="1.0.0")
 
