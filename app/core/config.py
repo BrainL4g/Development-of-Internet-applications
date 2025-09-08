24564a6677c835cf505db019f57f3eba8703b2cf
Egor <ifbytopic@gmail.com>
2025-09-08 20:25:39 +0700

    Removing

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-  database_url: str = "sqlite:///./db/test.db"
-
-
-settings = Settings()
--- app/core/dependencies.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from app.database import get_db
-from app.repositories.account import AccountRepository
-
-DbSession = Annotated[Session, Depends(get_db)]
-AccountRepo = Annotated[AccountRepository, Depends(lambda db: AccountRepository(db))]
